6146e0475e9f813e3685c7c0850c4d8be87f59c0
pandinh2007 <57869841+pandinh2007@users.noreply.github.com>
2024-07-16 10:54:04 +0700

    '1.0.0-first-version'

---
+++ modinfo.lua
@@ -0,0 +1,33 @@
+-- This information tells other players more about the mod
+name = "Pigking Don't Sleep"
+description = "Can't trade at night, lack of gold in light out mode? This is the solution"
+author = "dinhkarate"
+version = "11.111" -- This is the version of the template. Change it to your own number.
+forumthread = ""
+
+-- 1.4.5 fix tab
+
+
+-- This is the URL name of the mod's thread on the forum; the part after the ? and before the first & in the url
+
+-- This lets other players know if your mod is out of date, update it to match the current version in the game
+api_version = 10
+
+-- Compatible with Don't Starve Together
+dst_compatible = true
+server_filter_tags = {"dinhdeptrai", "dinhdeptraizzz", "dinhkarate", "vn", "vietnam"}
+-- Not compatible with Don't Starve
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+shipwrecked_compatible = false
+
+-- Character mods are required by all clients
+all_clients_require_mod = true 
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+-- The mod's tags displayed on the server list
+server_filter_tags = {
+"pigking", "lightout"
+}
