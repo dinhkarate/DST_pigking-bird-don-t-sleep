ccdba44fd42ac6c934ae3a2fb32519b3051eae3b
pandinh2007 <57869841+pandinh2007@users.noreply.github.com>
2024-07-21 15:30:32 +0700

    1.1

--- modmain.lua
+++ modmain.lua
@@ -1,3 +1,17 @@
 PrefabFiles = {
         "pigking"
 }
+
+
+
+local function RemoveSleeperComponent(inst)
+        if inst.components.sleeper then
+            inst:RemoveComponent("sleeper")
+        end
+    
+    end
+    
+    AddPrefabPostInit("crow", RemoveSleeperComponent)
+    AddPrefabPostInit("robin", RemoveSleeperComponent)
+    AddPrefabPostInit("robin_winter", RemoveSleeperComponent)
+    AddPrefabPostInit("canary", RemoveSleeperComponent)
